bce55cef94520e5615c56fa81379028c89c469b3
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-19 01:44:35 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,9 +5,9 @@ require_relative './fancy_dance.rb'
 class Kid
   
   
- # include Dance
+  include Dance
  #
- # attr_accessor :name
+  attr_accessor :name
  #
  # def initialize(name)
  #   @name = name
