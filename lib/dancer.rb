29e9c8add238b671fdf023894c9946d24b78bfe2
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-19 01:44:30 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,11 +4,11 @@ require_relative './fancy_dance.rb'
 
 class Dancer
   
- # include Dance
+  include Dance
  # 
  # extend MetaDancing
  # 
- # attr_accessor :name
+  attr_accessor :name
  #
  # def initialize(name)
  #   @name = name
