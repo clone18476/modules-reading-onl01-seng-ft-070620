ce05e2c9777a8ba3d0784a65e5505969183427e7
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-19 01:43:53 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,15 +5,15 @@ require_relative './fancy_dance.rb'
 class Kid
   
   
-  include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
-  
-  extend MetaDancing
+ # include Dance
+ #
+ # attr_accessor :name
+ #
+ # def initialize(name)
+ #   @name = name
+ # end
+ # 
+ # extend MetaDancing
   
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
