f65f9a9a989f423b544761a5e77fc55fdc45c11a
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-19 01:41:34 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
-#require_relative './fancy_dance.rb'
+require_relative './fancy_dance.rb'
 
 class Kid
   
@@ -15,7 +15,7 @@ class Kid
   
   extend MetaDancing
   
-  #extend FancyDance::ClassMethods
-  #include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   
 end
